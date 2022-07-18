03ca45150f86dd437c32460f90d571e0634cb2a3
mayorgeek <isaacadepoju2004@gmail.com>
2022-07-18 10:28:48 +0100

    feature: completed welcome screen

--- common/src/main/java/com/aiotouch/emojiquizapp/views/WelcomeScreen.java
+++ common/src/main/java/com/aiotouch/emojiquizapp/views/WelcomeScreen.java
@@ -1,31 +1,49 @@
 package com.aiotouch.emojiquizapp.views;
 
-import com.codename1.ui.Button;
-import com.codename1.ui.Dialog;
-import com.codename1.ui.FontImage;
-import com.codename1.ui.Form;
+import com.codename1.ui.*;
+import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.util.Resources;
+
+import java.io.IOException;
 
 public class WelcomeScreen extends Form {
 
     public WelcomeScreen() {
-        this.setTitle("Welcome Screen");
+        this.getToolbar().hideToolbar();
         this.setLayout(BoxLayout.y());
+        this.setScrollableY(false);
+        this.setUIID("WelcomeScreen");
         addComponents();
     }
 
     private void addComponents() {
-        Button helloButton = new Button("Hello Homepage");
-        this.add(helloButton);
+        Label welcomeImageContainer = new Label();
+        Image welcomeScreenImage = Resources.getGlobalResources().getImage("welcomeScreenImage.png");
+        welcomeScreenImage = welcomeScreenImage.scaledHeight(850);  // INCREASE IMAGE HEIGHT
+        welcomeImageContainer.setIcon(welcomeScreenImage);
+        welcomeImageContainer.setUIID("WelcomeImageContainer");
 
-        helloButton.addActionListener(e -> hello());
+        // ADD BUTTONS TO WELCOME SCREEN
+        Button startButton = new Button("Start Quiz");
+        startButton.setMaterialIcon(FontImage.MATERIAL_PLAY_CIRCLE_FILLED, 8);
+        startButton.setGap(48);
+        startButton.setUIID("StartButton");
 
-        this.getToolbar().addMaterialCommandToSideMenu("Hello Command",
-                FontImage.MATERIAL_CHECK, 4, e -> hello());
-    }
+        Button howToPlayButton = new Button("How to play?");
+        howToPlayButton.setMaterialIcon(FontImage.MATERIAL_HELP_OUTLINE, 8);
+        howToPlayButton.setGap(48);
+        howToPlayButton.setUIID("HowToPlayButton");
 
-    private void hello() {
-        Dialog.show("Hello Codename One", "Welcome to Codename One", "OK", null);
+        Button shareAppButton = new Button("Share this app");
+        shareAppButton.setMaterialIcon(FontImage.MATERIAL_SHARE, 6);
+        shareAppButton.setGap(48);
+        shareAppButton.setUIID("ShareAppButton");
+
+        this.add(welcomeImageContainer);
+        this.add(startButton);
+        this.add(howToPlayButton);
+        this.add(shareAppButton);
     }
 
 }
